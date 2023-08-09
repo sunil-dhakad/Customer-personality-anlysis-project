a44bd64eb808fc13b26ce6aee58138ceb9547728
sunil-dhakad <0to1.sunil@gmail.com>
2023-08-09 08:00:39 +0530

    adding setup file

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+from typing import List
+
+# Declaring Variables for setup functions
+PROJECT_NAME = "Customer-personality-anlysis-project"
+VERSION = "0.0.1"
+AUTHOR = "Sunil"
+DESCRIPTION = "This application predicts group of dataset"
+REQUIREMENT_FILE_NAME = "requirements.txt"
+
+def get_requirements_list()->List[str]:
+    """
+    Description: This function is going to return list of requirement mentioned in requirements.txt
+    """
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        return requirement_file.readlines().remove('-e .')
+
+setup(
+    name = PROJECT_NAME,
+    version=VERSION,
+    author=AUTHOR,
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=get_requirements_list()
+)
+
+if __name__=="__main__":
+    print(get_requirements_list())
+
